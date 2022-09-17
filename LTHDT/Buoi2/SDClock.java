fbb59681d62e598d3d8dbe3796ec3a3f390053f5
Huu Tai <112085653+Tylro@users.noreply.github.com>
2022-09-17 14:45:25 +0700

    Update SDClock.java

--- LTHDT/Buoi2/SDClock.java
+++ LTHDT/Buoi2/SDClock.java
@@ -2,53 +2,27 @@ package Buoi2;
 
 import java.util.Scanner;
 
-class Clock {
+public class SDClock {
 
-private int gio, phut, giay;
-
-public Clock() {
-	gio = 0; phut = 0; giay = 0;
-}
-public Clock(int h, int m, int s) {
-	gio = h; phut = m; giay = s; 
-}
-public void nhap() {
-	Scanner kb = new Scanner(System.in);
-	System.out.print("Nhap gio:");
-		gio = kb.nextInt();
-	System.out.print("Nhap phut:");
-		phut = kb.nextInt();
-	System.out.print("Nhap giay:");
-		giay = kb.nextInt();
-}
-
-public void hienThi() {
-	System.out.println(gio + ":" + phut + ":" + giay);
-}
-public void lamTron() {
-	int temp = gio * 3600 + phut * 60 + giay;
-	gio = temp / 3600;
-	phut = (temp % 3600) / 60;
-	giay = (temp % 3600) % 60;
-	if (gio >=24)
-		gio = gio- 24;
-}
-public Clock congPhut(int n) {
-	int temp = n * 60; 
-	gio = gio + (temp / 3600);
-	phut = phut + ((temp % 3600) / 60);
-	giay = giay + ((temp % 3600) % 60);
-	
-	return new Clock(gio, phut, giay);
-}
-public int giaTriGio() {
-	return gio;
-}
-public int giaTriPhut() {
-	return phut;
-}
-public int giaTriGiay() {
-	return giay;
-}
+	public static void main(String[] args) {
+		Clock c1 = new Clock(12,65,90);
+		System.out.println("Thoi gian c1:");
+		c1.hienThi();
+		c1.lamTron();
+		System.out.println("Thoi gian c1 sau khi lam tron:");
+		c1.hienThi();
+		Clock c2 = new Clock();
+		System.out.println("Nhap thoi gian c2:");
+		c2.nhap();
+		System.out.println("Thoi gian c2:");
+		c2.lamTron();
+		c2.hienThi();
+		System.out.print("Nhap n:");
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		c2.congPhut(n); 
+		c2.lamTron();
+		c2.hienThi();
+	}
 
 }
